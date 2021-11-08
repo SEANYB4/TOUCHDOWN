9829f5aac164bfe12d6907a606a80e7acda12ec9
Sean Bain <75031999+SEANYB4@users.noreply.github.com>
2021-11-08 16:07:30 +0000

    Initial Commit

---
+++ TOUCHDOWN/TOUCHDOWNApp.swift
@@ -0,0 +1,17 @@
+//
+//  TOUCHDOWNApp.swift
+//  TOUCHDOWN
+//
+//  Created by Sean Bain on 08/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct TOUCHDOWNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
